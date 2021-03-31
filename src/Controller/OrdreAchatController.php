077e5280ce536224694e746c08911dca9e5be8b8
Mathys-vende <56820126+Mathys-vende@users.noreply.github.com>
2021-03-31 07:23:10 +0200

    navabar

--- src/Controller/OrdreAchatController.php
+++ src/Controller/OrdreAchatController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/ordre/achat")
+ * @Route("/ordre_achat")
  */
 class OrdreAchatController extends AbstractController
 {
