ba3325bb89a270f4b32683f705198c809f1110d6
Mathys-vende <56820126+Mathys-vende@users.noreply.github.com>
2021-04-02 08:03:22 +0200

    Fix Home Controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,8 +3,7 @@
 namespace App\Controller;
 
 
-use App\Entity\Produit;
-use App\Entity\Produitcategorie;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,16 +15,10 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
-        /*$repo = $this->getDoctrine()->getRepository(Categorie::class);
-        $categorie = $repo->findAll();*/
 
-        $repo =$this->getDoctrine()->getRepository(Produit::class);
-        $idProd = $repo->findIdProduit(2);
-        //$repo = $this->getDoctrine()->getRepository(Produitcategorie::class);
 
 
         return $this->render('home/index.html.twig', [
-
             'controller_name' => 'HomeController',
         ]);
     }
